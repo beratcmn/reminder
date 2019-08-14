81299c276843abc1e628ffb3a7a4c29eab9eeab4
Berat Çimen <47108366+beratcmn@users.noreply.github.com>
2019-08-15 01:12:07 +0300

    Core Files
    Added core files.

---
+++ Source Code/reminder 0.2.py
@@ -0,0 +1,31 @@
+import time
+import ctypes
+import sys
+import os
+
+ctypes.windll.kernel32.SetConsoleTitleW("Reminder by Berat Çimen")
+os.system('mode con: cols=70 lines=20')
+remind = input("Neyin hatırlatılmasını istersiniz? ")
+
+while True:
+    x = input("Kaç dakika sonra hatırlatılsın? ")
+    try:
+        x = int(x)
+        break
+    except ValueError:
+        print("Geçersiz bir sayı değeri girdiniz. Lütfen tekrar deneyiniz.")
+        continue
+
+
+sure = x * 60 
+while  sure > 0:
+    dakika = int(sure / 60)
+    saniye = int(sure % 60)
+    #print(dakika,saniye)
+    baslik = str(dakika) + " : " + str(saniye)
+    ctypes.windll.kernel32.SetConsoleTitleW(str(baslik))
+    time.sleep(1)
+    sure = sure - 1
+
+ctypes.windll.user32.MessageBoxW(0, "'" + remind + "'" + " adlı hatırlatıcının süresi sona erdi.", "Reminder", 0)
+sys.exit()
